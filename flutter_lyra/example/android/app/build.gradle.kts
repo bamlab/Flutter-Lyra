0e05d581d9b03642c652c57a9f488f5b2ebf9f0c
MaximeRougieux <maximer@bam.tech>
2025-09-15 10:29:47 +0200

    refactor: :truck: rename example package for coherence

--- flutter_lyra/example/android/app/build.gradle.kts
+++ flutter_lyra/example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_lyra_example"
+    namespace = "tech.bam.flutter_lyra_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_lyra_example"
+        applicationId = "tech.bam.flutter_lyra_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
